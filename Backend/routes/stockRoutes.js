a4bd173449c397246e6f8349ec91140316e86937
Guilherme  Rodrigues <devguirf11@gmail.com>
2023-04-22 23:53:20 +0000

    adicionando rotas

--- Backend/routes/stockRoutes.js
+++ Backend/routes/stockRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import { Estoque } from '../models/Estoque.js'
 
 const stockRoutes = Router();
 
-stockRoutes.post('/api/cadastrarPeca', async (req, res) => {
+stockRoutes.post('/cadastrarPecas', async (req, res) => {
     try {
         const { peca_descricao, peca_modelo, peca_valorRevenda, peca_valorCompra, peca_marca } = req.body;
         const novaPeca = await Estoque.create({ peca_descricao, peca_modelo, peca_valorRevenda, peca_valorCompra, peca_marca });
@@ -12,6 +13,22 @@ stockRoutes.post('/api/cadastrarPeca', async (req, res) => {
         console.error(error);
         res.status(500).json({ message: 'Erro ao cadastrar peça.' });
       }
+})
+
+stockRoutes.get('/listaPecas', async (req, res) => {
+
+  try {
+    const pecas = await Estoque.findAll()
+    if (pecas) {
+      res.status(200).send(
+        pecas
+      )
+    }
+    
+  } catch (error) {
+    console.error(error);
+      res.status(500).json({ message: 'Erro ao listar peças.' });
+  }
 })
 
 export default stockRoutes;
